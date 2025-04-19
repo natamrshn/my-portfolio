9c60caf4238cf00319e8d447ac17a2c696c67a04
natamrshn <natamrshn@gmail.com>
2025-04-19 10:08:36 +0100

    add exp info

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -8,23 +8,37 @@ export interface Job {
 
 export const jobs: Job[] = [
   {
-    company: "Selfbook",
-    title: "Software Developer (Remote)",
-    location: "Selfbook / US - New York",
-    date: "Jun 2021 - Present",
+    company: "Nordic Nest",
+    title: "Frontend Developer",
+    location: "Remote",
+    date: "Jan 2025 - Present",
     tasks: [
-      "Developing screens and UI components for the web application using React and Tailwind.",
-      "Fixing UI issues and integrating backend APIs with Redux Saga.",
+      "Created modular and reusable React components using TypeScript, ensuring scalability and maintainability across the project.",
+      "Developed a Single Page Application (SPA) to enhance user experience by reducing page load times and ensuring smooth navigation.",
+      "Implemented responsive, cross-device layouts using CSS Flexbox and Grid to ensure a seamless experience on various screen sizes.",
     ],
   },
   {
-    company: "Upwork",
-    title: "Freelance Frontend Developer",
+    company: "Skarpette",
+    title: "Frontend Developer",
     location: "Remote",
-    date: "Feb 2020 - Jun 2021",
+    date: "Mar 2024 - Jan 2025",
     tasks: [
-      "Worked with various clients building web UIs with React.",
-      "Created reusable components and optimized frontend performance.",
+      "Built and optimized React components with TypeScript, focusing on code reusability and performance.",
+      "Implemented SPA architecture to improve overall application performance and reduce load times.",
+      "Enhanced user experience through lazy loading, API optimizations, and responsive design techniques.",
+      "Integrated RESTful APIs for seamless data management and smooth user interactions. Also integrated Nova Poshta API for order creation.",
+    ],
+  },
+  {
+    company: "Optik Okulist",
+    title: "Markup Developer",
+    location: "Remote",
+    date: "Jan 2024 - Mar 2024",
+    tasks: [
+      "Built responsive page layouts using Flexbox, styled and animated them with BEM methodology and SASS for better code structure and scalability.",
+      "Created page skeletons and structure using semantic HTML, focusing on accessibility, SEO optimization, and clean component hierarchy.",
+      "Worked on hover effects, CSS transitions, and mobile-first responsiveness to ensure smooth and adaptive user experience.",
     ],
   },
 ];
